9ecaf2838aeb8fc305e6338e951f6d74b0c76c15
susuuBin <azxq1000@gmail.com>
2020-07-08 13:11:16 +0900

    result kakaotalk, 링크 수정.

--- js/result_kakaotalk.js
+++ js/result_kakaotalk.js
@@ -7,16 +7,16 @@ function shareKakaotalk() {
             , description:"The Starry Night ⭐ 당신의 화가를 찾아 보세요."   // 콘텐츠 상세 설명
             , imageUrl:'https://kangeunseo.github.io/TheStarryNight/images/thumbnail.jpg'   // 썸네일 이미지
             , link : {
-                  mobileWebUrl:"https://kangeunseo.github.io/TheStarryNight/index_m.html"   // 모바일 카카오톡에서 사용하는 웹 링크 URL
-                , webUrl:"https://kangeunseo.github.io/TheStarryNight/index.html" // PC버전 카카오톡에서 사용하는 웹 링크 URL
+                  mobileWebUrl:"https://kangeunseo.github.io/TheStarryNight/index.html"   // 모바일 카카오톡에서 사용하는 웹 링크 URL
+                , webUrl:"https://kangeunseo.github.io/TheStarryNight/index_d.html" // PC버전 카카오톡에서 사용하는 웹 링크 URL
             }
         }
         , buttons : [
             {
                   title:"테스트 하러 가기"    // 버튼 제목
                 , link : {
-                    mobileWebUrl:"https://kangeunseo.github.io/TheStarryNight/index_m.html"   // 모바일 카카오톡에서 사용하는 웹 링크 URL
-                  , webUrl:"https://kangeunseo.github.io/TheStarryNight/index.html" // PC버전 카카오톡에서 사용하는 웹 링크 URL
+                    mobileWebUrl:"https://kangeunseo.github.io/TheStarryNight/index.html"   // 모바일 카카오톡에서 사용하는 웹 링크 URL
+                  , webUrl:"https://kangeunseo.github.io/TheStarryNight/index_f.html" // PC버전 카카오톡에서 사용하는 웹 링크 URL
                 }
             }
         ]
